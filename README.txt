e0548b62acc874f1960f3c6f82b0cb78b1da3034
truewheeldon <truewheeldon@gmail.com>
2019-03-04 08:39:10 -0600

    Initial commit

---
+++ README.txt
@@ -0,0 +1,18 @@
+README.txt
+True Wheeldon 
+3/3/19
+
+todo: 
+- data verification of user input type(variable)
+	if the type is wrong variable, notify user
+- make console output more tolerable to look at
+- finish readme instructions
+
+either: 'pip install -r requirements.txt'
+
+or install separately:
+pip install click
+pip install requests
+
+install python 3.7+
+
---
+++ query.py
@@ -0,0 +1,36 @@
+import click
+import requests
+import json
+
+
+@click.group()
+def main():
+    pass
+
+
+@main.command()
+@click.option("--albumid", prompt="Album (1-50)", help="Choose a photo album from 1-50.")
+def hello(albumid):
+
+    # check = type(albumid)   # user input valid?
+    # if not check.isdigit():  # is not currently functioning, would be nice to have
+    #     print("Please run this program again and enter a number from 1-50.")
+    #     exit()
+
+    url_format = "https://jsonplaceholder.typicode.com/photos"
+
+    if albumid:     # put only the right chunk of JSON in album object
+        query_params = {"albumId": albumid}
+        response = requests.get(url_format, params=query_params)
+    else:           # otherwise make entire album object the JSON placeholder
+        response = requests.get(url_format)
+
+    album_object = json.loads(response.text)
+
+    for album in album_object:       # print to console
+        statement = "id: {} | title: {}".format(album["id"], album["title"])
+        click.echo(statement)
+
+
+if __name__ == "__main__":
+    main()
